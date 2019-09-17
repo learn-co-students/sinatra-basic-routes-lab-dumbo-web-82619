3988aa242812bcf8c494e4d48d7ed5018d19853f
Karina Macancela <karymacancela@gmail.com>
2019-09-17 11:57:57 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+    
+    get '/name' do
+        "My name is Karina"
+      end
+    
+      get '/hometown' do
+        "My hometown is Queens, NY"
+      end
+    
+      get '/favorite-song' do
+        "My favorite song is anything indie usually"
+      end
 end
